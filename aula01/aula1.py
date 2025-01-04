44a4f84560e44fc9ca0c3e2e6d00ea1d4f3333b3
noelnunesds <noelnunescosta@gmail.com>
2025-01-04 16:58:09 -0300

    Criação pasta aula01

---
+++ aula01/aula1.py
@@ -0,0 +1,30 @@
+# git init inicializa o git
+# git add . 
+
+print("Hello World!")
+
+print( 3 + 4**2 )
+
+# Exercícios
+# 1. Crie um programa que o usuário digite seu nome e retorne a quantidade de caracteres
+nome = input(f'Olá, qual é o seu nome?:')
+
+print(f'A quantidade de caracteres no seu nome é: {len(nome)}')
+
+# 2. Crie um programa que o usuário digite, dois valores e apareça a soma
+def soma(a, b):
+    return a + b 
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print(soma(n1, n2))
+
+numero = 3 # int
+numero_float = 3.0
+texto_string = "string"
+listas = [1,2,3,4]
+tuplas = (1, 2)
+dicts = {"noel": 'gostoso'}
+booleanos = (True or False)
+
+# Desafio
