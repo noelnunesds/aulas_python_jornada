d0438f48c3216906e8db885031035abbf2522044
noelnunesds <noelnunescosta@gmail.com>
2025-01-10 18:43:22 -0300

    added_files_for_etl

---
+++ aula06_etl_pandas/etl.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os 
+import glob 
+
+
+
+# Função de extract que le e consolida os json
+pasta = 'data'
+arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
+df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+
+print(df_list)
+
+#pd.read_json(path_or_buf=)
+
+# Uma função que transforma
+
+#load
